3bc8b95a55c2ef43545b59ee7bea7966fedf4746
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-08 02:20:19 +0600

    problem 3 solved with for loop

--- src/solutions.ts
+++ src/solutions.ts
@@ -12,3 +12,12 @@ function formatString(input: string, toUpper?: boolean): string{
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
     return items.filter((item) => item.rating >= 4)
 }
+
+
+function concatenateArrays<T>(...arrays: T[][]): T[]{
+    const result: T[] = []
+    for(const array of arrays){
+        result.push(...array)
+    }
+    return result
+}
