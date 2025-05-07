8c1840df88ef000cc2fc75e197d84225d622a9ba
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-08 02:03:39 +0600

    problem 2 solved

--- src/solutions.ts
+++ src/solutions.ts
@@ -6,3 +6,9 @@ function formatString(input: string, toUpper?: boolean): string{
         return input.toUpperCase()
     }
 }
+
+
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+    return items.filter((item) => item.rating >= 4)
+}
