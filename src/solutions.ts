01622d4d957ab39ee570103c0d26c4c2382021cf
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-08 02:37:11 +0600

    problem 4 solved

--- src/solutions.ts
+++ src/solutions.ts
@@ -21,3 +21,27 @@ function concatenateArrays<T>(...arrays: T[][]): T[]{
     }
     return result
 }
+
+
+
+class Vehicle{
+    private make: string;
+    private year: number;
+    constructor(make: string, year: number){
+        this.make = make
+        this.year = year
+    }
+    getInfo(){
+        return `Make: ${this.make}, Year: ${this.year}`
+    }
+}
+class Car extends Vehicle{
+    private model: string
+    constructor(make: string, year: number, model: string){
+        super(make, year)
+        this.model = model
+    }
+    getModel(){
+        return `Model: ${this.model}`
+    }
+}
